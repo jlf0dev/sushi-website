67285d1792f254f2af020d8456634c8529fb4bfc
Jacob Fink <jfink@bitwarden.com>
2022-01-24 19:23:17 -0500

    accessibility changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
           <div className="flex-grow flex flex-col items-center justify-center max-w-[90%] mx-auto z-10">
             <h1 className="text-8xl mb-7 font-heading">Sushi</h1>
             <p className="text-2xl mb-7 text-center font-light">Let your senses run riot with our tantalizing asian fusion.</p>
-            <button className="bg-[#ffba00] py-4 px-11 uppercase hover:bg-black transition-colors duration-700 ease-in-out font-bold">See Menu</button>
+            <button className="bg-[#ffba00] text-black py-4 px-11 uppercase hover:bg-black transition-colors duration-700 ease-in-out font-bold">See Menu</button>
           </div>
         </div>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <html lang="en" />
         <meta name="description" content="A template website built with nextjs"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
